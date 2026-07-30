6908de1ec3de32e6ea97c851d2dd05a3668fea44
SATHISH IT <sathishkpr2006@gmail.com>
2026-07-30 21:39:29 +0530

    Update prediction API endpoint URL

--- frontend/js/predict.js
+++ frontend/js/predict.js
@@ -13,7 +13,7 @@ async function predict() {
     document.getElementById("result").innerHTML = "Predicting...";
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("http://recruitment-candidate-screening.onrender.com/predict"), {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
