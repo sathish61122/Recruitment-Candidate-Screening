300e8cda304051c635c62fdbb7703855ff339701
SATHISH IT <sathishkpr2006@gmail.com>
2026-09-16 22:02:58 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,8 +2,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import joblib
-
-# Load dataset
 df = pd.read_csv("candidates.csv")
 
 # Features & target
