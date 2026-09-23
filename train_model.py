b248576988a667ebc72788303760e622c517e479
SATHISH IT <sathishkpr2006@gmail.com>
2026-09-23 10:21:55 +0530

    Fix formatting issue in train_model.py

--- train_model.py
+++ train_model.py
@@ -10,6 +10,7 @@ y = df['selected']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
 # Train model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
