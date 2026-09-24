1c1107419d0b03d01c5173f5b66bc45f6dc806ab
SATHISH IT <sathishkpr2006@gmail.com>
2026-09-24 22:20:41 +0530

    Modify CORS middleware settings
    Updated CORS middleware configuration.

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # after deployment you can restrict
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
